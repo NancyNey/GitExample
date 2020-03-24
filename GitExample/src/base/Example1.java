8309c05fe1430f28ad810b28cdbdc5fe83569917
Nancy <nancygotta5@gmail.com>
2020-03-24 09:57:21 +0100

    Aggiunto metodo che ritorna Stringa

--- GitExample/src/base/Example1.java
+++ GitExample/src/base/Example1.java
@@ -3,8 +3,11 @@ package base;
 public class Example1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println(metodo1());
 
 	}
-
+	public static String metodo1()
+	{
+		return "Hello world";
+	}
 }
